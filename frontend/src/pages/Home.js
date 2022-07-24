13b70961261aab0751f9d7cdaa65ce8b3bf2215e
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-07-24 02:04:27 -0300

    feat: fetching API data, workout details component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import WorkoutDetails from "../components/WorkoutDetails";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts"); //can use like this cause i defined a proxy at package.json (fixes CORS error)
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
