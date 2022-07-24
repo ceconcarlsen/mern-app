70478612e6d648f6370c8cfb328e122015ef8683
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-07-24 19:49:03 -0300

    feat: validations at signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,17 @@
 const User = require("../models/UserModel");
 
 //login user
-const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
-};
+const loginUser = async (req, res) => {};
 
 //signup user
 const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password); //static method
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
