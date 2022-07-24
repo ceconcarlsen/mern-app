c5b08767238164c25a66aea472f7b70b515e7c39
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-07-24 19:31:52 -0300

    feat: authentication routes, controller and model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Workout = require("../models/WorkoutModel");
 const {
   createWorkout,
   getWorkouts,
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connection to database
 mongoose
