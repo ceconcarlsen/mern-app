9c3ec54926001e9c5a6321afe2ba1048d84d943a
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-07-25 11:15:47 -0300

    feat: jwt token when signing up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,31 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_KEY, { expiresIn: "3d" });
+};
 
 //login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const { token, user } = await User.signin(email, password); //static method
+    res.status(200).json({ email, user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 //signup user
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password); //static method
-    res.status(200).json({ email, user });
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -14,6 +15,27 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+//static signup method
+userSchema.statics.signin = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email does not exists");
+  }
+
+  const user = await this.findOne({ email, password });
+
+  return { user };
+};
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
